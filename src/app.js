6b8cc84576e8989c43a69062a977dd7894ba7a86
User <user@users-mbp.home>
2020-02-18 21:14:36 +0000

    committing hw

--- src/app.js
+++ src/app.js
@@ -4,28 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data:{
-      rates: []
-
+      rates: [],
+            base: "GBP",
+            baseInput: 0,
+            selected: "EUR",
+    },
+    computed: {
+      exchanged: function() {
+        const result = this.baseInput * this.rates[this.selected];
+        return result.toFixed(2);
+      },
     },
     mounted(){
       this.fetchRates();
     },
     methods:{
       fetchRates: function(){
-        const request = fetch('https://api.exchangeratesapi.io/latest')
+        const request = fetch(`https://api.exchangeratesapi.io/latest?base=${this.base}`)
         .then(response => response.json())
-        .then(data => this.rates = data);
-
+        .then(data =>
+          // this.rates = console.log(data.rates));
+          this.rates = data.rates);
 
       },
-      // getCountries: function(){
-//   fetch("https://restcountries.eu/rest/v2/all")
-//   .then(res => res.json())
-//   .then(countries => this.countries = countries)
-// },
     }
-  })
-})
-    // });
-  // });
-// });
+  });
+});
